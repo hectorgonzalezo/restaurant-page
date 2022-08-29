74740621f5be5347686baf7fead6b88ba67469a4
Hector Gonzalez <gorozco.hector@gmail.com>
2022-08-29 17:07:08 -0400

    Refactor menu module

--- src/menu.js
+++ src/menu.js
@@ -9,35 +9,23 @@ const menu = (function () {
     pizzasArea.id = 'pizzas-area';
     const pizzasText = document.createElement('h3');
     pizzasText.innerText = 'Our pizzas';
+    pizzasArea.append(pizzasText);
+    
+    function _appendPizza(url, text) {
+        const img= new Image();
+        img.src = url;
+        const textDisplay = document.createElement('p');
+        textDisplay.innerText = text;
+        pizzasArea.append(img, textDisplay)
+    }
 
-    const margheritaImg = new Image();
-    margheritaImg.src = margheritaUrl;
-    const margheritaText = document.createElement('p');
-    margheritaText.innerText = 'Mozzarella, tomato and basil';
-
-    const prosciuttoImg = new Image();
-    prosciuttoImg.src = prosciuttoUrl;
-    const prosciuttoText = document.createElement('p');
-    prosciuttoText.innerText = 'Ham and mushroom';
-
-    const formaggiImg = new Image();
-    formaggiImg.src = formaggiUrl;
-    const formaggiText = document.createElement('p');
-    formaggiText.innerText = 'Provolone, parmigiano, mozarella and goat cheese';
-
-    const veggieImg = new Image();
-    veggieImg.src = veggieUrl;
-    const veggieText = document.createElement('p');
-    veggieText.innerText = 'Bell peppers, zuchini and capers';
-
-
+    _appendPizza(margheritaUrl, 'Mozzarella, tomato and basil');
+    _appendPizza(prosciuttoUrl, 'Ham and mushroom');
+    _appendPizza(formaggiUrl, 'Provolone, parmigiano, mozarella and goat cheese')
+    _appendPizza(veggieUrl, 'Bell peppers, Zucchini and capers'); 
 
     function render(main) {
-        pizzasArea.append(pizzasText, 
-            margheritaImg, margheritaText,
-            prosciuttoImg, prosciuttoText,
-            formaggiImg, formaggiText,
-            veggieImg, veggieText);
+
         main.append(pizzasArea);
     }
 
