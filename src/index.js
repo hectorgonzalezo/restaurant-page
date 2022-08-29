bb4fd84c283d67bab9b56972d7847e41f9a1fdf2
Hector Gonzalez <gorozco.hector@gmail.com>
2022-08-29 15:55:19 -0400

    Deactivate reclick on header options

--- src/index.js
+++ src/index.js
@@ -3,13 +3,3 @@ import webpage from './createWebpage.js'
 
 webpage.render('home');
 
-
-function addOptionsListeners(){
-    const headerOptions = document.querySelectorAll('.header-option');
-    headerOptions.forEach((option) => {
-        const data = option.getAttribute('data');
-        option.addEventListener('click', () => webpage.updateMain(data))
-    })
-};
-
-addOptionsListeners();
